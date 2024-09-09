cf23efd26ade0fa3632710df01746029ed520fc9
Jaime Tejeiro <Jaimetejeiro@gmail.com>
2024-09-09 11:30:04 +0200

    Initial Commit

---
+++ TheHotel/TheHotelApp.swift
@@ -0,0 +1,32 @@
+//
+//  TheHotelApp.swift
+//  TheHotel
+//
+//  Created by Jaime Tejeiro on 9/9/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TheHotelApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
